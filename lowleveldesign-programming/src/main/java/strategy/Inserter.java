db6756141bc3c2532b7b1b994b8ed67ce57bc65b
dineshans <dineshsahuans@gmail.com>
2024-01-15 00:01:32 +0530

    removed

--- lowleveldesign-programming/src/main/java/strategy/Inserter.java
+++
@@ -1,7 +0,0 @@
-package strategy;
-
-import java.util.List;
-
-public interface Inserter {
-    public void insert(int num, List<Integer> nums);
-}
--- lowleveldesign-programming/src/main/java/strategy/Searcher.java
+++
@@ -1,7 +0,0 @@
-package strategy;
-
-import java.util.List;
-
-public interface Searcher {
-    public Integer search(int key, List<Integer> nums);
-}
